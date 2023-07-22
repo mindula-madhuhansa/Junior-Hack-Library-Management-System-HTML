0559a47b091f05fdcdc181dfd23b14f628a0a5a9
harshana2001 <1778harsh2001@gmail.com>
2023-07-22 11:28:18 +0530

    Inserted updateBookByISBN

--- HTML/src/BookSQL.java
+++ HTML/src/BookSQL.java
@@ -1,40 +1,27 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
 public class BookSQL {
-
-    public void insertBook(Book book) {
-        PreparedStatement psInsert = null;
-
-        DBConnection connectNow = new DBConnection();
-        Connection connectDB = connectNow.getDBConnection();
-
-        String createNewBookQuery = "INSERT INTO record (isbn, title, category, author) VALUES (?, ?, ?, ?)";
-
-        try {
-            psInsert = connectDB.prepareStatement(createNewBookQuery);
-            psInsert.setLong(1, book.getIsbn());
-            psInsert.setString(2, book.getTitle());
-            psInsert.setString(3, book.getCategory());
-            psInsert.setString(4, book.getAuthor());
-            int r = psInsert.executeUpdate();
-
-            if(r > 0){
-                System.out.println("Successfully created a Book object");
-
-            } else {
-                System.out.println("Error while Creating the Book object");
-
-            }
-        } catch (SQLException ex) {
-            throw new RuntimeException(ex);
-        }
+    public void insertBook() {
 
     }
 
-    public void updateBookByISBN() {
+    public void updateBookByISBN(Book book) {
 
+        DBConnection connection = new DBConnection();
+        Connection connectDB = connection.getDBConnection();
+        String query = "UPDATE library SET(title = ? ,category = ?, author = ?) WHERE isbn is equal to"+book.getIsbn()+" )";
+
+        try {
+
+            PreparedStatement statement = connectDB.prepareStatement(query);
+            statement.setString(1,book.getTitle());
+            statement.setString(2,book.getCategory());
+            statement.setString(3,book.getAuthor());
+            statement.executeUpdate();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     public void deleteBookByISBN() {
