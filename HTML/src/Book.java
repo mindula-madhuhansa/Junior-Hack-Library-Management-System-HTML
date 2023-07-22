40dd8322db3c988ed1b86ae408f29fe37e2d256a
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-07-22 10:42:58 +0530

    add addBook menu

--- HTML/src/Book.java
+++ HTML/src/Book.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Book {
     private long isbn;
     private String title;
     private String category;
     private String author;
 
+    public Book() {
+    }
 
     public Book(long isbn, String title, String category, String author) {
         this.isbn = isbn;
@@ -30,6 +34,38 @@ public class Book {
 
     public void setAuthor(String author) {
         this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "isbn=" + isbn +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public void addBook(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add a new book");
+
+        System.out.print("Enter the ISBN: ");
+        String newIsbn = scanner.nextLine();
+        setIsbn(Long.parseLong(newIsbn));
+
+        System.out.print("Enter the title: ");
+        String newTitle = scanner.nextLine();
+        setTitle(newTitle);
+
+        System.out.print("Enter the category");
+        String newCategory = scanner.nextLine();
+        setTitle(newCategory);
+
+        System.out.print("Enter the author");
+        String newAuthor = scanner.nextLine();
+        setTitle(newAuthor);
+
     }
 
 }
--- HTML/src/Main.java
+++ HTML/src/Main.java
@@ -1,6 +1,62 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to ");
+        System.out.println("Welcome to HTML library management system UoK!");
+        menuMethod();
+
+    }
+
+    public static void menuMethod() {
+        System.out.println("Please choose an option:");
+        System.out.println("1. Add a new book.");
+        System.out.println("2. Update an existing book.");
+        System.out.println("3. Remove an book.");
+        System.out.println("4. Lend a book.");
+        System.out.println("5. Return a book.");
+        System.out.println("6. List all available books.");
+        System.out.println("7. Exit.");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int inputNum = scanner.nextInt();
+
+        switch (inputNum) {
+            case 1:
+                Book newBook = new Book();
+                newBook.addBook();
+                newBook.toString();
+                break;
+//
+//            case 2:
+//                updateBook();
+//                break;
+//
+//            case 3:
+//                removeBook();
+//                break;
+//
+//            case 4:
+//                lendBook();
+//                break;
+//
+//            case 5:
+//                returnBook();
+//                break;
+//
+//            case 6:
+//                listAllBook();
+//                break;
+//
+//            case 7:
+//                //add exit code
+//                break;
+
+            default:
+                // keep looping
+                break;
+
+        }
     }
 }
