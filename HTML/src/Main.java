c034601dab01e646c91799f3213c6bdea120967f
Mindula Madhuhansa <76749525+mindula-madhuhansa@users.noreply.github.com>
2023-07-22 12:33:51 +0530

    finished

--- HTML/src/Main.java
+++ HTML/src/Main.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 
 
 public class Main {
-    private ArrayList<Book> bookList = new ArrayList<Book>();
+    private static ArrayList<Book> bookList = new ArrayList<Book>();
+    static Book newBook = new Book();
 
     public static void main(String[] args) {
         System.out.println("Welcome to HTML library management system UoK!");
@@ -24,30 +25,29 @@ public class Main {
             System.out.print("Enter the number: ");
             int inputNum = scanner.nextInt();
 
-            Book newBook = new Book();
             switch (inputNum) {
                 case 1:
-                    newBook.addBook();
+                    addBook();
                     break;
 
                 case 2:
-                    newBook.updateBook();
+                    updateBook();
                     break;
 
                 case 3:
-                    newBook.removeBook();
+                    removeBook();
                     break;
 
                 case 4:
-                    newBook.lendBook();
+                    lendBook();
                     break;
 
                 case 5:
-                    newBook.returnBook();
+                    returnBook();
                     break;
 
                 case 6:
-                    newBook.listAllBooks();
+                    listAllBooks();
                     break;
 
                 case 7:
@@ -66,4 +66,125 @@ public class Main {
     }
 
 
+    public static void addBook(){
+        Book book = new Book();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add a new book");
+
+        System.out.print("Enter the ISBN: ");
+        String newIsbn = scanner.nextLine();
+        book.setIsbn(Long.parseLong(newIsbn));
+
+        System.out.print("Enter the title: ");
+        String newTitle = scanner.nextLine();
+        book.setTitle(newTitle);
+
+        System.out.print("Enter the category: ");
+        String newCategory = scanner.nextLine();
+        book.setCategory(newCategory);
+
+        System.out.print("Enter the author: ");
+        String newAuthor = scanner.nextLine();
+        book.setAuthor(newAuthor);
+
+        bookList.add(book);
+        System.out.println( bookList.get(0));
+
+//        BookSQL.insertBook(book);
+    }
+
+
+    public static void updateBook(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Update an existing book");
+        System.out.print("Enter the ISBN that need to update: ");
+        long isbnU = scanner.nextLong();
+        System.out.println(bookList.size());
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).getIsbn() == isbnU){
+                System.out.println("Update the book: ");
+
+                System.out.println("Enter the new Title " + bookList.get(i).getTitle() + ": ");
+                String updatedTitle = scanner.nextLine();
+                newBook.setTitle(updatedTitle);
+
+                System.out.println("Enter the new Category " +bookList.get(i).getCategory() + ": ");
+                String updatedCategory = scanner.nextLine();
+                newBook.setCategory(updatedCategory);
+
+                System.out.println("Enter the new Author " + bookList.get(i).getAuthor() + ": ");
+                String updatedAuthor = scanner.nextLine();
+                newBook.setAuthor(updatedAuthor);
+
+            }else{
+                System.out.println("Wrong ISBN");
+            }
+
+        }
+
+
+
+    }
+
+    public static void removeBook(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Remove an existing book");
+
+        System.out.print("Enter the ISBN: ");
+        long isbnRemove = scanner.nextLong();
+        if (newBook.getIsbn() == isbnRemove){
+            System.out.println("Are you sure you want to remove this book? (Y/N)");
+            String confirmation = scanner.nextLine();
+            switch (confirmation){
+                case "Y":
+                    // do the removing
+                    break;
+                case "N":
+                    // cancel the process
+                    break;
+                default:
+                    System.out.println("Enter a valid option (Y/N)");
+                    break;
+            }
+        }
+
+    }
+
+    public static void lendBook(){
+        Book lendBook = new Book();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Lending a book");
+
+        System.out.print("Enter the ISBN: ");
+        long isbnLend = scanner.nextLong();
+        if (isbnLend ==  newBook.getIsbn()){
+            System.out.println("Can proceed the lending process");
+        }
+        else {
+            System.out.println("This book is not available right now.");
+        }
+
+
+    }
+
+    public static void returnBook(){
+        Book returnBook = new Book();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Return a book");
+
+        System.out.print("Enter the ISBN: ");
+        long isbnReturn = scanner.nextLong();
+        if (isbnReturn == newBook.getIsbn()){
+            System.out.println("Book returned successfully");
+        }
+        else {
+            System.out.println("Enter a valid ISBN");
+        }
+
+    }
+
+    public static void listAllBooks(){
+        System.out.println("List all available books");
+        // list all book function
+    }
 }
